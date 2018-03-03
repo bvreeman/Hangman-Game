e3a8317946b5a15aaf3cff92991273496c3a641e
bvreeman <bvreeman@gmail.com>
2018-03-03 14:41:17 -0600

    changing buttons to javascript loop with variable

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,16 +4,36 @@ const wordBank = ["cat", "crocodile", "human", "fish"];
 
 const chosenWord = wordBank [Math.floor((Math.random()*wordBank.length))];
 
-// alert(chosenWord);
+console.log(chosenWord);
 
 let chosenWordLength = chosenWord.length;
 
-// alert(chosenWordLength);
+console.log(chosenWordLength);
 
 let dashWordLength = [];
 
-for(let dashCounter=0; dashCounter <= chosenWordLength.length; dashCounter++); {
+for(let dashCounter=0; dashCounter < chosenWordLength; dashCounter++) {
     dashWordLength.push('_');
-}
 
-// alert(dashWordLength);
+    console.log(dashWordLength);
+
+};
+
+const letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "_"];
+
+for (const i = 0; i < letters.length; i++) {
+    const letterBtn = $("<button>");
+    letterBtn.addClass("letter-button letter letter-button-color");
+    letterBtn.attr("data-letter", letters[i]);
+    letterBtn.text(letters[i]);
+    $("#buttons").append(letterBtn);
+
+};
+
+// function pictureChange()
+// {
+// document.getElementById('btn-letter').src=("./assets/images/paw_print.png");
+// };
+
+
+
