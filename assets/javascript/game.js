49d377b645acf6e177c7a251f67eced2c90e2a05
bvreeman <bvreeman@gmail.com>
2018-03-03 16:17:24 -0600

    got the main area ligned up again

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -17,16 +17,49 @@ for(let dashCounter=0; dashCounter < chosenWordLength; dashCounter++) {
 
     console.log(dashWordLength);
 
+
+
 };
 
-const letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "_"];
+comments = function () {
+    guessesRemaining.innerHTML = "You have " + guesses + " remaining";
+    if (lives < 1) {
+      guessesRemaining.innerHTML = "Game Over";
+    }
+    for (let i = 0; i < geusses.length; i++) {
+      if (counter + space === geusses.length) {
+        guessesRemaining.innerHTML = "You Win!";
+      }
+    }
+  }
+const letters1 = ["A", "B", "C", "D", "E", "F", "G", "H", "I"];
+const letters2 = ["J", "K", "L", "M", "N", "O", "P", "Q", "R"];
+const letters3 = [ "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
-for (const i = 0; i < letters.length; i++) {
-    const letterBtn = $("<button>");
+for (let i = 0; i < letters1.length; i++) {
+    let letterBtn = $("<button>");
     letterBtn.addClass("letter-button letter letter-button-color");
-    letterBtn.attr("data-letter", letters[i]);
-    letterBtn.text(letters[i]);
-    $("#buttons").append(letterBtn);
+    letterBtn.attr("data-letter", letters1[i]);
+    letterBtn.text(letters1[i]);
+    $("#buttons1").append(letterBtn);
+
+};
+
+for (let i = 0; i < letters2.length; i++) {
+    let letterBtn = $("<button>");
+    letterBtn.addClass("letter-button letter letter-button-color");
+    letterBtn.attr("data-letter", letters2[i]);
+    letterBtn.text(letters2[i]);
+    $("#buttons2").append(letterBtn);
+
+};
+
+for (let i = 0; i < letters3.length; i++) {
+    let letterBtn = $("<button>");
+    letterBtn.addClass("letter-button letter letter-button-color");
+    letterBtn.attr("data-letter", letters3[i]);
+    letterBtn.text(letters3[i]);
+    $("#buttons3").append(letterBtn);
 
 };
 
