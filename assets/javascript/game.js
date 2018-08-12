e21e0055874ea132851f4f05651602defd023d35
bvreeman <bvreeman@gmail.com>
2018-08-11 21:46:52 -0500

    corrected the error with the multiple alert message

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -20,12 +20,11 @@ let remainingLetters=chosenWord.length;
 
 $(document).on('keypress', function(event) {
     const guess = event.key;
-    // if (!(event.which <= 90 && event.which >= 65)) return;
 
     if(chosenWord.includes(guess)) {
         correctGuess(guess);
     } 
-
+    
     if (guess) {
         guessesRemaining--;
         document.querySelector('#myLives').innerHTML=guessesRemaining;
@@ -33,31 +32,27 @@ $(document).on('keypress', function(event) {
         if (guessesRemaining === 0){
             alert("You Lose!");
         }
-        console.log(guessesRemaining);
 });
 
-function correctGuess(guess)
-{
+function correctGuess(guess) {
     const chosenWordLetters = chosenWord.split('');
-    let eventLocations = [];
+     const eventLocations = [];
     for (let i = 0; i < chosenWord.length; i++){
         if(chosenWordLetters[i] === guess){
             eventLocations.push(i)
             remainingLetters--;
         } 
-        if(remainingLetters === 0){
-            alert("You Win!");
-        }
     } 
+    if(remainingLetters === 0){
+        alert("You Win!");
+    }
 
     for(let i = 0; i < eventLocations.length; i++){
         dashWordLength[eventLocations[i]]=guess;
     }
     
     document.querySelector('#word').innerHTML=dashWordLength.join(' ')
-
 };
-
 
 let keysPressed = [""];
 document.onkeydown = function(event){
@@ -65,8 +60,6 @@ document.onkeydown = function(event){
     let key_code = event.keyCode;
     keysPressed.push(key_press);
     document.querySelector("#guessedLetters").innerHTML=(keysPressed).join(" ");
-//    console.log(keysPressed);
-   
 };
 
 
@@ -81,99 +74,11 @@ function reset(){
     document.querySelector('#word').innerHTML=dashWordLength.join(' ');
     }
 
-        $('#guessedLetters').empty();
-        keysPressed.length=0;
-        remainingLetters=chosenWord.length;
-        guessesRemaining=15;
-        document.querySelector('#myLives').innerHTML=(guessesRemaining);
-
-
+    $('#guessedLetters').empty();
+    keysPressed.length=0;
+    remainingLetters=chosenWord.length;
+    guessesRemaining=15;
+    document.querySelector('#myLives').innerHTML=(guessesRemaining);
 };
 
 $('#reset').on('click', reset);
-
-// Guesses Remaining section
-
-  
-//   comments = function () {
-//     showLives.innerHTML = "You have " + lives + " lives";
-//     if (lives < 1) {
-//       showLives.innerHTML = "Game Over";
-//     }
-//     for (var i = 0; i < geusses.length; i++) {
-//       if (counter + space === geusses.length) {
-//         showLives.innerHTML = "You Win!";
-//       }
-//     }
-//   }
-
-
-// let answerArray = [];
-//     for (let i = 0; i < chosenWord.length;) {
-//         answerArray[i] = "_";
-//     }
-// let remainingLetters = wordBank.length;
-
-// while (remainingLetters > 0) {
-//     alert(answerArray.join(" "));
-// }
-
-// 
-
-
-// const letters1 = ["A", "B", "C", "D", "E", "F", "G", "H", "I"];
-// const letters2 = ["J", "K", "L", "M", "N", "O", "P", "Q", "R"];
-// const letters3 = [ "S", "T", "U", "V", "W", "X", "Y", "Z"];
-
-// for (let i = 0; i < letters1.length; i++) {
-//     let letterBtn = $("<button>");
-//     letterBtn.addClass("letter-button letter letter-button-color");
-//     letterBtn.attr("data-letter", letters1[i]);
-//     letterBtn.text(letters1[i]);
-//     $("#buttons1").append(letterBtn);
-//     onclick= "visibility = hidden";
-
-// };
-
-// for (let i = 0; i < letters2.length; i++) {
-//     let letterBtn = $("<button>");
-//     letterBtn.addClass("letter-button letter letter-button-color");
-//     letterBtn.attr("data-letter", letters2[i]);
-//     letterBtn.text(letters2[i]);
-//     $("#buttons2").append(letterBtn);
-
-// };
-
-// for (let i = 0; i < letters3.length; i++) {
-//     let letterBtn = $("<button>");
-//     letterBtn.addClass("letter-button letter letter-button-color");
-//     letterBtn.attr("data-letter", letters3[i]);
-//     letterBtn.text(letters3[i]);
-//     $("#buttons3").append(letterBtn);
-
-// };
-
-// let guessesRemaining ;
-// let showLives = document.getElementById("myGuesses");
-
-// comments = function () {
-//     guessesRemaining.innerHTML = "You have " + guesses + " remaining";
-//     if (lives < 1) {
-//       guessesRemaining.innerHTML = "Game Over";
-//     }
-//     for (let i = 0; i < geusses.length; i++) {
-//       if (counter + space === geusses.length) {
-//         guessesRemaining.innerHTML = "You Win!";
-//       }
-//     }
-// };
-    
-
-
-// function pictureChange()
-// {
-// document.getElementById('buttons1').src=("./assets/images/paw_print.png");
-// };
-
-
-
